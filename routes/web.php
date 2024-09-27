3f6db806d95ab4fbe25c434dd8fd92e1ee771bf4
Sebastián Camargo <juansecapri@hotmail.com>
2024-09-27 14:09:28 -0500

    Registro deshabilitado

--- routes/web.php
+++ routes/web.php
@@ -24,10 +24,10 @@ Route::get('/', function () {
 Route::get('/form', [FormResponseController::class, 'create']);
 Route::post('/form', [FormResponseController::class, 'store']);
 
-Route::get('/admin/register', function () {
-    return view('admin.register');
-});
-Route::post('/admin/register', [AdminRegisterController::class, 'register'])->name('admin.register.submit');
+// Route::get('/admin/register', function () {
+//     return view('admin.register');
+// });
+// Route::post('/admin/register', [AdminRegisterController::class, 'register'])->name('admin.register.submit');
 
 Route::get('/admin/login', function () {
     return view('admin.login');
